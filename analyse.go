2477ea35020b752aaf03bd339886481e87743393
Sajith Silva <sajiths@pagero.com>
2018-07-12 11:02:53 +0530

    more updates

--- analyse.go
+++ analyse.go
@@ -1,16 +1,21 @@
 package main
 
 import (
-    "flag"
-	"bufio"
+	"flag"
 	"fmt"
 	"log"
 	"os"
-	"regexp"
 	"gopkg.in/src-d/go-git.v4"
 	"github.com/mgutz/ansi"
 	"os/exec"
+	"bufio"
+	"regexp"
+	"sync"
+	"time"
+	"strconv"
 )
+
+var wg sync.WaitGroup
 
 type Repo struct {
 	url    string
@@ -25,6 +30,10 @@ func PadRight(str, pad string, lenght int) string {
 			return str[0:lenght]
 		}
 	}
+}
+
+func red() func(string) string {
+	return ansi.ColorFunc("red+")
 }
 
 func green() func(string) string {
@@ -49,10 +58,10 @@ func printRepo(r []Repo) {
 func updateRepos(r []Repo, noUpdate bool) {
 	fmt.Printf("\n\n")
 	for _, repo := range r {
-		if (repo.status == true)  { //update
+		if (repo.status == true) { //update
 
 			if (noUpdate) {
-				fmt.Printf(yellow()("Skip update of %s\n"),repo.folder)
+				fmt.Printf(red()("Skip update of %s\n"), repo.folder)
 			} else {
 				fmt.Printf(green()("Updating repository %s\n"), repo.folder)
 				repo, _ := git.PlainOpen("/home/sajith/stats/" + repo.folder)
@@ -72,7 +81,7 @@ func updateRepos(r []Repo, noUpdate bool) {
 }
 
 func printStats(r []Repo, user string, from string, to string) {
-	fmt.Printf(green()("\nGenerating statistics for user:%s \t From %s  To %s\n"),user, from, to)
+	fmt.Printf(green()("\nGenerating statistics for user:%s \t From %s  To %s\n"), user, from, to)
 	for _, repo := range r {
 		fmt.Printf(yellow()(repo.folder + "___________________________________________________________________________\n"))
 		output, err := exec.Command("/home/sajith/scratch/gitstats/getstat.sh", repo.folder, "kanishka.desilva@pagero.com", from, to).Output()
@@ -91,38 +100,63 @@ func getRepoStatus(stats bool) string {
 	}
 }
 
+func worker(output chan string, i int) {
+	defer wg.Done()
+	var r int = 2 * i
+	fmt.Printf("running id %d with time %d\n", i, r)
+	time.Sleep(time.Duration(r) * time.Second)
+	fmt.Printf("done id %d with time %d\n", i, r)
+	output <- "Woke up after " + strconv.Itoa(r)
+}
+
+func xxx(workers int) {
+	var output = make(chan string, 100)
+	for i := 0; i < workers; i++ {
+		wg.Add(1)
+		go worker(output, i)
+	}
+
+	wg.Wait()
+	close(output)
+
+	for result := range output {
+		fmt.Println(result)
+	}
+}
+
 func main() {
 
-    var noUpdate bool
+	var noUpdate bool
 	var user string
 	var from string
 	var to string
 
-    flag.BoolVar(&noUpdate, "noupdate", true, "a string var")
-    flag.StringVar(&user, "user", "", "a string var")
-    flag.StringVar(&from, "from", "", "a string var")
-    flag.StringVar(&to, "to", "", "a string var")
+	flag.BoolVar(&noUpdate, "noupdate", true, "a string var")
+	flag.StringVar(&user, "user", "", "a string var")
+	flag.StringVar(&from, "from", "", "a string var")
+	flag.StringVar(&to, "to", "", "a string var")
 	flag.Parse()
 
-    fmt.Println("user:", user)
+	fmt.Println("user:", user)
 	fmt.Println("from:", from)
-	fmt.Println("to:",to)
-	fmt.Println("noUpdate:",noUpdate)
-	
+	fmt.Println("to:", to)
+	fmt.Println("noUpdate:", noUpdate)
+
 	file, err := os.Open("git_repos.txt")
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	defer file.Close()
+	defer file.Close() //defer so it cleanup.
 
 	fmt.Printf(green()("Repositories....\n"))
 	var repoList = make([]Repo, 0)
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		re, _ := regexp.Compile(`[a-z]+\.(git)`)
+		//re, _ := regexp.Compile(`[a-z]+\.(git)`)
+		re, _ := regexp.Compile(`(\w+-)?\w+\.git`)
 		repo := scanner.Text()
 		res := re.FindAllStringSubmatch(repo, -1)
 
@@ -139,11 +173,14 @@ func main() {
 	}
 
 	printRepo(repoList)
+	xxx(3)
+
+	/*
 	updateRepos(repoList, noUpdate)
 	fmt.Print(green()("\nAll repos are update\n"))
 
 	if len(user) > 0 {
 		printStats(repoList, user, from, to)
-	}
+	}*/
 
 }
--- getstat.sh
+++ getstat.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cd /home/sajith/stats
 cd $1
-git log --author=$2 --since $3 --until $4 --oneline --shortstat | gawk '{ add += $1; subs += $2; loc += $1 - $2 } END { printf "added lines: %s removed lines: %s total lines: %s\n", add, subs, loc }' -
+auth=$3
+git log --author=$2 --since $3 --until $4 --numstat --pretty=tformat:  --no-merges| gawk '{ add += $1; subs += $2; loc += $1 - $2;name=$auth} END { 
+printf "{\"name\":\"%s\",\n\"added\": %s,\n\"removed\": %s,\n\"total\": %s}\n", name,add, subs, loc }' 
